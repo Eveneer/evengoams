8fb0e5af1ad33d6f422c5a52edbfba726954a703
Mobashir Monim <mobashirmonim@gmail.com>
2024-07-09 20:39:52 +0600

    Add after validator for create transaction

--- app/Domains/Transactions/Actions/CreateTransaction.php
+++ app/Domains/Transactions/Actions/CreateTransaction.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App\Domains\Transactions\Actions;
 
+use App\Domains\Accounts\Account;
+use App\Domains\Donors\Donor;
+use App\Domains\Employees\Employee;
+use App\Domains\RevenueStreams\RevenueStream;
 use App\Domains\Transactions\Transaction;
 use App\Domains\Transactions\Enums\TransactionTypesEnum;
+use App\Domains\Vendors\Vendor;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -37,15 +43,33 @@ class CreateTransaction
             'amount' => ['required', 'integer', 'min:0'],
             'author_id' => ['required', 'exists:users,id'],
             'type' => ['required', 'in:' . implode(',', TransactionTypesEnum::asArray())],
-            'fromable_type' => ['required', 'string', 'in:RevenueStream,Donor,Account'],
+            'fromable_type' => [
+                'required',
+                'in:' . implode(',', [Account::class, Donor::class, RevenueStream::class])
+            ],
             'fromable_id' => ['required', 'uuid'],
-            'toable_type' => ['required', 'string', 'in:Employee,Vendor,Account'],
+            'toable_type' => [
+                'required',
+                'in:' . implode(',', [Account::class, Employee::class, Vendor::class])
+            ],
             'toable_id' => ['required', 'uuid'],
             'parent_id' => ['nullable', 'uuid', 'exists:transactions,id'],
             'note' => ['nullable', 'string'],
             'tag_ids' => ['nullable', 'json'],
             'is_last' => ['boolean'],
         ];
+    }
+
+    public function afterValidator(Validator $validator, ActionRequest $request): void
+    {
+        $fromable = $request->fromable_type;
+        $toable = $request->toable_type;
+
+        if ($fromable::find($request->fromable_id) === null)
+            $validator->errors()->add('fromable_id', 'Invalid fromable selected');
+
+        if ($toable::find($request->toable_id) === null)
+            $validator->errors()->add('toable_id', 'Invalid toable selected');
     }
 
     public function asController(Request $request)
