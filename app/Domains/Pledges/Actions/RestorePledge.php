635c7738152679f4b3a91e5f95835726a8a0d26e
Mahir12746 <mahiraseef@gmail.com>
2024-07-09 13:10:36 +0600

    write validation rules for restore pledge

--- app/Domains/Pledges/Actions/RestorePledge.php
+++ app/Domains/Pledges/Actions/RestorePledge.php
@@ -35,7 +35,7 @@ class RestorePledge
     public function rules(): array
     {
         return [
-            'id' => ['exists:']
+            'id' => ['required', 'exists:pledges,id'],
         ];
     }
 
