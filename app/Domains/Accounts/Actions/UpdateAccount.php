2f36443909063c96ab2d5d24f7ee918d5be8a8bd
Mobashir Monim <mobashirmonim@gmail.com>
2024-07-09 17:07:30 +0600

    Minor bugfix on account update validation rule

--- app/Domains/Accounts/Actions/UpdateAccount.php
+++ app/Domains/Accounts/Actions/UpdateAccount.php
@@ -34,7 +34,7 @@ class EditAccount
     public function rules(): array
     {
         return [
-            'id' => ['required', 'uuid'],
+            'id' => ['required', 'exists:accounts,id'],
             'name' => ['sometimes', 'string', 'max:255'],
             'balance' => ['sometimes', 'integer', 'min:0'],
             'type' => ['sometimes', 'in:' . implode(',', AccountTypesEnum::asArray())],
