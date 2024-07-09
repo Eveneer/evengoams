ce43fc97bf063d43d46f234604e618b95cb2df78
Mahir12746 <mahiraseef@gmail.com>
2024-07-09 12:19:28 +0600

    write validation rules for create vendor

--- app/Domains/Vendors/Actions/CreateVendor.php
+++ app/Domains/Vendors/Actions/CreateVendor.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domains\Vendors\Actions;
 
 use App\Domains\Vendors\Vendor;
+use App\Domains\Vendors\Enums\VendorTypesEnum;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 use Lorisleiva\Actions\ActionRequest;
@@ -32,7 +33,12 @@ class CreateVendor
     public function rules(): array
     {
         return [
-
+            'name' => ['required', 'string', 'max:255'],
+            'type' => ['required', 'in:' . implode(',', VendorTypesEnum::asArray())],
+            'tag_ids' => ['nullable', 'array'],
+            'tag_ids.*' => ['uuid'],
+            'contacts' => ['nullable', 'array'],
+            'contacts.*' => ['string', 'max:255'],
         ];
     }
 
