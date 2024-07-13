bf188f312225b6eea1e2bf1eefc88036475c342d
Mahir12746 <mahiraseef@gmail.com>
2024-07-13 19:36:17 +0600

    fix validation for create-vendor

--- app/Domains/Vendors/Actions/CreateVendor.php
+++ app/Domains/Vendors/Actions/CreateVendor.php
@@ -36,9 +36,11 @@ class CreateVendor
             'name' => ['required', 'string', 'max:255'],
             'type' => ['required', 'in:' . implode(',', VendorTypesEnum::asArray())],
             'tag_ids' => ['nullable', 'array'],
-            'tag_ids.*' => ['uuid'],
+            'tag_ids.*' => ['exists:tags,id'],
             'contacts' => ['nullable', 'array'],
-            'contacts.*' => ['string', 'max:255'],
+            'contacts.*.name' => ['nullable', 'string'],
+            'contacts.*.phone' => ['nullable', 'string'],
+            'contacts.*.email' => ['nullable', 'email'],
         ];
     }
 
