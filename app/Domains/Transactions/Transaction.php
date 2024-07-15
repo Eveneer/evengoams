2e13e20e42c0ab8bd342dc2082eb2345d7d0229a
Mahir12746 <mahiraseef@gmail.com>
2024-07-15 14:48:04 +0600

    write model relations for transaction

--- app/Domains/Transactions/Transaction.php
+++ app/Domains/Transactions/Transaction.php
@@ -2,11 +2,56 @@
 
 namespace App\Domains\Transactions;
 
+use App\Domains\Users\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'date',
+        'amount',
+        'author_id',
+        'type',
+        'fromable_type',
+        'fromable_id',
+        'toable_type',
+        'toable_id',
+        'parent_id',
+        'note',
+        'tag_ids',
+        'is_last',
+    ];
+
+    protected $casts = [
+        'tag_ids' => 'array',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function fromable()
+    {
+        return $this->morphTo();
+    }
+
+    public function toable()
+    {
+        return $this->morphTo();
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
