8d1670401c27a21fc698223e7dbae3d14ccde418
Mahir12746 <mahiraseef@gmail.com>
2024-07-15 15:14:01 +0600

    write model relations for donor

--- app/Domains/Donors/Donor.php
+++ app/Domains/Donors/Donor.php
@@ -2,11 +2,27 @@
 
 namespace App\Domains\Donors;
 
+use App\Domains\Transactions\Transaction;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Donor extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'occupation',
+        'title',
+        'company',
+    ];
+
+    public function transactions()
+    {
+        return $this->morphMany(Transaction::class, 'fromable');
+    }
 }
