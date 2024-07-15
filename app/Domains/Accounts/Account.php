0969a63238d888bcd567b8075fee7d818265f68a
Mahir12746 <mahiraseef@gmail.com>
2024-07-15 14:22:11 +0600

    write model relations for account

--- app/Domains/Accounts/Account.php
+++ app/Domains/Accounts/Account.php
@@ -2,11 +2,33 @@
 
 namespace App\Domains\Accounts;
 
+use App\Domains\Transactions\Transaction;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'balance',
+        'type',
+        'details',
+    ];
+
+    protected $casts = [
+        'details' => 'array',
+    ];
+
+    public function fromableTransactions()
+    {
+        return $this->morphMany(Transaction::class, 'fromable');
+    }
+
+    public function toableTransactions()
+    {
+        return $this->morphMany(Transaction::class, 'toable');
+    }
 }
