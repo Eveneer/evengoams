7e79a2f8701eb94399a708f4bb016bc92c9562d4
Mahir12746 <mahiraseef@gmail.com>
2024-07-15 15:07:34 +0600

    write model relations for vendor

--- app/Domains/Vendors/Vendor.php
+++ app/Domains/Vendors/Vendor.php
@@ -2,11 +2,29 @@
 
 namespace App\Domains\Vendors;
 
+use App\Domains\Transactions\Transaction;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Vendor extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'type',
+        'tag_ids',
+        'contacts',
+    ];
+
+    protected $casts = [
+        'tag_ids' => 'array',
+        'contacts' => 'array',
+    ];
+
+    public function transactions()
+    {
+        return $this->morphMany(Transaction::class, 'toable');
+    }
 }
