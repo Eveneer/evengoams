10385a51ec85a24ce540d05979a6a69fb8434b93
Mahir12746 <mahiraseef@gmail.com>
2024-07-16 15:28:55 +0600

    write model relation for transaction v2

--- app/Domains/Transactions/Transaction.php
+++ app/Domains/Transactions/Transaction.php
@@ -2,13 +2,14 @@
 
 namespace App\Domains\Transactions;
 
+use App\Domains\Tags\Tag;
 use App\Domains\Users\User;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Transaction extends Model
 {
@@ -56,5 +57,10 @@ class Transaction extends Model
     public function child(): HasOne
     {
         return $this->hasOne(self::class, 'parent_id');
+    }
+
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
     }
 }
