ecfece1454de710ad7598e347144944b2eae69e2
Mahir Aseef <86533931+Mahir12746@users.noreply.github.com>
2024-07-17 20:33:47 +0600

    2024 07 15/mahir/tag model relations (#56)
    * Closes #20
    
    * write model relations for tag
    
    * write model relations for tag v2
    
    * write model relation for tag v3
    
    * update code of tag model
    
    * Update vendor and transaction relationship in tag model
    
    ---------
    
    Co-authored-by: Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>

--- app/Domains/Tags/Tag.php
+++ app/Domains/Tags/Tag.php
@@ -2,11 +2,37 @@
 
 namespace App\Domains\Tags;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
 
 class Tag extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'key',
+        'model',
+    ];
+
+    public function model(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function transactions(): Builder
+    {
+        return DB::table('transaction')
+            ->where('tag_ids', 'LIKE', "%$this->id%");
+    }
+
+    public function vendors(): Builder
+    {
+        return DB::table('vendor')
+            ->where('tag_ids', 'LIKE', "%$this->id%");
+    }
 }
