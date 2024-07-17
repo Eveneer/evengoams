9e96da5c2f6c84502021d9fd976a611718ece21e
Mahir Aseef <86533931+Mahir12746@users.noreply.github.com>
2024-07-17 20:34:13 +0600

    2024 07 15/mahir/vendor model relations (#52)
    * Close #18
    
    * write model relations for vendor
    
    * Update return of vendor transaction relationship function
    
    * write model relation for vendor v2
    
    * update code of vendor model
    
    * Update tag relationship in vendor mdoel
    
    * Remove redundant dependency injections in vendor model
    
    ---------
    
    Co-authored-by: Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>

--- app/Domains/Vendors/Vendor.php
+++ app/Domains/Vendors/Vendor.php
@@ -2,11 +2,40 @@
 
 namespace App\Domains\Vendors;
 
+use App\Domains\Tags\Enums\TagModelsEnum;
+use App\Domains\Transactions\Transaction;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
 
 class Vendor extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'type',
+        'tag_ids',
+        'contacts',
+    ];
+
+    protected $casts = [
+        'tag_ids' => 'array',
+        'contacts' => 'array',
+    ];
+
+    public function transactions(): MorphMany
+    {
+        return $this->morphMany(Transaction::class, 'toable');
+    }
+
+    public function tags(): Builder
+    {
+        return DB::table('tags')
+            ->where('model', TagModelsEnum::VENDOR)
+            ->whereIn('id', $this->tag_ids);
+    }
 }
