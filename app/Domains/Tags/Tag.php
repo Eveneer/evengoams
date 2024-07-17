a77185d1d513b6979de42fbebb89b72f0a7ee438
Mahir12746 <mahiraseef@gmail.com>
2024-07-17 14:23:55 +0600

    update code of tag model

--- app/Domains/Tags/Tag.php
+++ app/Domains/Tags/Tag.php
@@ -3,10 +3,12 @@
 namespace App\Domains\Tags;
 
 use App\Domains\Vendors\Vendor;
-use App\Domains\Transactions\Transaction;
 use Illuminate\Database\Eloquent\Model;
+use App\Domains\Transactions\Transaction;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Tag extends Model
 {
@@ -18,17 +20,17 @@ class Tag extends Model
         'model',
     ];
 
-    public function model()
+    public function model(): MorphTo
     {
         return $this->morphTo();
     }
 
-    public function transactions()
+    public function transactions(): MorphToMany
     {
         return $this->morphedByMany(Transaction::class, 'taggable');
     }
 
-    public function vendors()
+    public function vendors(): MorphToMany
     {
         return $this->morphedByMany(Vendor::class, 'taggable');
     }
