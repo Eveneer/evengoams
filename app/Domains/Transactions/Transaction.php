67e098a37da5a82da2d8ab8d7545e1deb626647a
Mahir Aseef <86533931+Mahir12746@users.noreply.github.com>
2024-07-17 20:33:59 +0600

    2024 07 15/mahir/transaction model relations (#54)
    * Closes #14
    
    * write model relations for transaction
    
    * Update relationships in transaction model
    
    * write model relation for transaction v2
    
    * update code of transaction model
    
    * Update tag relationship in transaction
    
    ---------
    
    Co-authored-by: Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>

--- app/Domains/Transactions/Transaction.php
+++ app/Domains/Transactions/Transaction.php
@@ -2,11 +2,69 @@
 
 namespace App\Domains\Transactions;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Domains\Tags\Enums\TagModelsEnum;
+use App\Domains\Users\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
 
 class Transaction extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'date',
+        'amount',
+        'author_id',
+        'type',
+        'fromable_type',
+        'fromable_id',
+        'toable_type',
+        'toable_id',
+        'parent_id',
+        'note',
+        'tag_ids',
+        'is_last',
+    ];
+
+    protected $casts = [
+        'tag_ids' => 'array',
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function from(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function to(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function child(): HasOne
+    {
+        return $this->hasOne(self::class, 'parent_id');
+    }
+
+    public function tags(): Builder
+    {
+        return DB::table('tags')
+            ->where('model', TagModelsEnum::TRANSACTION)
+            ->whereIn('id', $this->tag_ids);
+    }
 }
