6fee83f61889274c035afec3f52999c0bfeb7c80
Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>
2024-07-23 02:44:51 +0600

    Wip employees and transactions factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            AccountSeeder::class,
+            DonorSeeder::class,
+            VendorSeeder::class,
+            // TransactionSeeder::class
         ]);
     }
 }
