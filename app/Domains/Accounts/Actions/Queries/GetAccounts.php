ad2660623af4eff991dc43efd4080b24a86e3199
Mahir12746 <mahiraseef@gmail.com>
2024-07-30 19:36:13 +0600

    write functions for getaccounts

--- app/Domains/Accounts/Actions/Queries/GetAccounts.php
+++ app/Domains/Accounts/Actions/Queries/GetAccounts.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 namespace App\Domains\Accounts\Actions\Queries;
 
+use Illuminate\Http\Request;
 use App\Domains\Accounts\Account;
+use Lorisleiva\Actions\ActionRequest;
+use Illuminate\Support\Facades\Response;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class GetAccounts
 {
     use AsAction;
 
+    public function authorize(ActionRequest $request): Response
+    {
+        $user = $request->user();
+        
+        if ($user->has_general_access)
+            return Response::allow();
+
+        return Response::deny('You are unauthorised to perform this action');
+    }
+
     public function handle()
     {
         return Account::paginate(10);
+    }
+
+    public function asController(Request $request)
+    {
+        return $this->handle($request->validated());
     }
 }
