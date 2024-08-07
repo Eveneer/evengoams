a36c3d5a989c46510823e4598c8a99832c12cb62
Mahir12746 <mahiraseef@gmail.com>
2024-08-07 15:28:33 +0600

    write model relations for revenuestream

--- app/Domains/RevenueStreams/RevenueStream.php
+++ app/Domains/RevenueStreams/RevenueStream.php
@@ -2,11 +2,29 @@
 
 namespace App\Domains\RevenueStreams;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use App\Domains\RevenueStreamTypes\RevenueStreamType;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RevenueStream extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'type_id',
+        'values',
+    ];
+
+    protected $casts = [
+        'values' => 'array',
+    ];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(RevenueStreamType::class, 'type_id');
+    }
 }
