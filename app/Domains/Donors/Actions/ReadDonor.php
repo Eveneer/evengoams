8dc9dca8ca74eb7f9fb5ea14c413673b90e1225e
Mahir12746 <mahiraseef@gmail.com>
2024-08-13 15:05:23 +0600

    write read action for donor

---
+++ app/Domains/Donors/Actions/ReadDonor.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\Donors\Actions;
+
+use App\Domains\Donors\Donor;
+use Illuminate\Support\Facades\Response;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ReadDonor
+{
+    use AsAction;
+
+    public function authorize(ActionRequest $request): Response
+    {
+        $user = $request->user();
+        
+        if ($user->has_general_access) {
+            return Response::allow();
+        }
+
+        return Response::deny('You are unauthorized to perform this action');
+    }
+
+    public function handle(string $id): Donor
+    {
+        return Donor::findOrFail($id);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'id' => ['required', 'exists:donors,id'],
+        ];
+    }
+
+    public function asController(ActionRequest $request)
+    {
+        return $this->handle($request->get('id'));
+    }
+
+    public function jsonResponse(Donor $donor): array
+    {
+        return [
+            'data' => $donor,
+            'message' => 'Donor retrieved successfully',
+        ];
+    }
+}
