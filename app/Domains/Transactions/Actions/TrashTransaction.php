d1ff026974239f8923855c7a00b9d2f225424f39
Mahir12746 <mahiraseef@gmail.com>
2024-08-20 15:31:44 +0600

    update trashtransaction action

--- app/Domains/Transactions/Actions/TrashTransaction.php
+++ app/Domains/Transactions/Actions/TrashTransaction.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domains\Transactions\Actions;
 
-use App\Domains\Transactions\Transaction;
-use Illuminate\Support\Facades\Response;
+use App\Domains\Accounts\Account;
 use Lorisleiva\Actions\ActionRequest;
+use Illuminate\Support\Facades\Response;
+use App\Domains\Transactions\Transaction;
 use Lorisleiva\Actions\Concerns\AsAction;
+use App\Domains\Accounts\Actions\AddBalance;
 
 class TrashTransaction
 {
@@ -25,6 +27,23 @@ class TrashTransaction
 
     public function handle(Transaction $transaction): bool
     {
+        if ($transaction->fromable_type === Account::class) {
+            AddBalance::run([
+                'id' => $transaction->fromable_id, 
+                'amount' => $transaction->amount
+            ]);
+        }
+
+        if ($transaction->toable_type === Account::class) {
+            AddBalance::run([
+                'id' => $transaction->toable_id, 
+                'amount' => -1 * $transaction->amount
+            ]);
+        }
+        
+        $transaction->tags()->detach();
+        
+        
         return $transaction->delete();
     }
 
