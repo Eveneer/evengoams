4f081206919fabfc10e14367595e379060dd3123
Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>
2024-09-02 11:54:46 +0600

    Update dev tool and get account action

--- app/Domains/Accounts/Actions/Queries/GetAccount.php
+++ app/Domains/Accounts/Actions/Queries/GetAccount.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Domains\Accounts\Actions\Queries;
 
 use App\Domains\Accounts\Account;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Auth\Access\Response;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 
