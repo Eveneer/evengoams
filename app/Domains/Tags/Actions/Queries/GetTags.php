36d1c911c2d8a817b1372c29f06837f214017bdf
Mahir12746 <mahiraseef@gmail.com>
2024-09-04 11:44:42 +0600

    update parameters passed to handle of GetTags

--- app/Domains/Tags/Actions/Queries/GetTags.php
+++ app/Domains/Tags/Actions/Queries/GetTags.php
@@ -57,7 +57,7 @@ class GetTags
         $search_term = $request->input('search_term');
         $per_page = $request->input('per_page');
 
-        return $this->handle($search_term, $per_page);
+        return $this->handle($per_page, $search_term);
     }
 
     public function jsonResponse(array $tags, ActionRequest $request): array
