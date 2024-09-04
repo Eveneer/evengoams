f9e36e73b9f2749bf46470536668587570dc2665
Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>
2024-09-04 17:27:09 +0600

    Update account routes

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-require_once __DIR__ . '/domains/accounts.php';
+require __DIR__ . '/domains/accounts.php';
--- routes/domains/accounts.php
+++ routes/domains/accounts.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->prefix('accounts')->group(function (): void {
     Route::get('/', GetAccounts::class);
     Route::get('/{id}', GetAccount::class);
-    Route::post('/create', CreateAccount::class);
-    Route::put('/update', UpdateAccount::class);
+    Route::post('/', CreateAccount::class);
+    Route::put('/{id}', UpdateAccount::class);
     Route::post('/{id}/trash', TrashAccount::class);
     Route::post('/{id}/restore', RestoreAccount::class);
 });
