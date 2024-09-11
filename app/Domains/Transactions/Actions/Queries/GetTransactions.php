928606ecbc96c8e55a6b48dde8d6051df948b916
Mahir12746 <mahiraseef@gmail.com>
2024-09-12 00:41:58 +0600

    Modify GetTransactions for api route

--- app/Domains/Transactions/Actions/Queries/GetTransactions.php
+++ app/Domains/Transactions/Actions/Queries/GetTransactions.php
@@ -7,6 +7,7 @@ namespace App\Domains\Transactions\Actions\Queries;
 use App\Domains\Accounts\Actions\Queries\GetAccounts;
 use App\Domains\Donors\Actions\Queries\GetDonors;
 use App\Domains\Tags\Actions\Queries\GetTags;
+use App\Domains\Tags\Tag;
 use App\Domains\Transactions\Transaction;
 use App\Domains\Vendors\Actions\Queries\GetVendors;
 use Illuminate\Auth\Access\Response;
@@ -33,29 +34,28 @@ class GetTransactions
     public function handle(?int $per_page = 10, ?string $search_term = ''): Collection | LengthAwarePaginator
     {
         $transactions = Transaction::query();
-        
-        $tags = (new GetTags())->handle($search_term);
+        $tags = (new GetTags())->handle($per_page, $search_term);
 
-        $donors = (new GetDonors())->handle($search_term);
+        // $donors = (new GetDonors())->handle($per_page, $search_term);
 
-        $vendors = (new GetVendors())->handle($search_term);
+        // $vendors = (new GetVendors())->handle($search_term);
 
-        $accounts = (new GetAccounts())->handle($search_term);
+        // $accounts = (new GetAccounts())->handle($per_page, $search_term);
         
         if ($search_term) {
             $transactions->where('note', 'like', "%$search_term%")
                 ->orWhereHas('tags', function ($query) use ($tags) {
                     $query->whereIn('id', $tags->pluck('id'));
-                })
-                ->orWhereHas('donor', function ($query) use ($donors) {
-                    $query->whereIn('id', $donors->pluck('id'));
-                })
-                ->orWhereHas('vendor', function ($query) use ($vendors) {
-                    $query->whereIn('id', $vendors->pluck('id'));
-                })
-                ->orWhereHas('account', function ($query) use ($accounts) {
-                    $query->whereIn('id', $accounts->pluck('id'));
                 });
+                // ->orWhereHas('donor', function ($query) use ($donors) {
+                //     $query->whereIn('id', $donors->pluck('id'));
+                // })
+                // ->orWhereHas('vendor', function ($query) use ($vendors) {
+                //     $query->whereIn('id', $vendors->pluck('id'));
+                // })
+                // ->orWhereHas('account', function ($query) use ($accounts) {
+                //     $query->whereIn('id', $accounts->pluck('id'));
+                // });
         }
     
         return $per_page === null ?
@@ -73,18 +73,15 @@ class GetTransactions
 
     public function asController(ActionRequest $request)
     {
-        $search_term = $request->input('search_term');
-        $per_page = $request->input('per_page');
-
-        return $this->handle($search_term, $per_page);
+        return $this->handle($request->per_page, $request->search_term);
     }
 
     public function jsonResponse(
-        array $transactions,
+        Collection | LengthAwarePaginator $transactions,
         ActionRequest $request
     ): array {
         $message = count($transactions) . ' transactions ';
-        $message .= $request->input('search_term') ? 'found' : 'fetched';
+        $message .= $request->search_term ? 'found' : 'fetched';
 
         return [
             'data' => $transactions,
