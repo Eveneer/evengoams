33bdb745b5b873888992d63d85e393651820f682
Mahir12746 <mahiraseef@gmail.com>
2024-09-15 00:58:37 +0600

    write GetEmployees action with api route

--- routes/domains/employees.php
+++ routes/domains/employees.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use App\Domains\Employees\Actions\Queries\GetEmployee;
+use App\Domains\Employees\Actions\Queries\GetEmployees;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->prefix('employees')->group(function (): void {
+    Route::get('/', GetEmployees::class);
     Route::get('/{id}', GetEmployee::class);
 });
