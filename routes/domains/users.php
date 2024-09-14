1b918ce0fa5000c3a782e35019917a4c27325e3b
Mahir12746 <mahiraseef@gmail.com>
2024-09-15 00:28:59 +0600

    write GetUsers action with api route

--- routes/domains/users.php
+++ routes/domains/users.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use App\Domains\Users\Actions\Queries\GetUser;
+use App\Domains\Users\Actions\Queries\GetUsers;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->prefix('users')->group(function (): void {
+    Route::get('/', GetUsers::class);
     Route::get('/{id}', GetUser::class);
+
 });
