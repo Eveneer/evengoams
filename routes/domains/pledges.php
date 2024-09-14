ab88a134a81dfda0709c67416cd72465ad748a6c
Mahir12746 <mahiraseef@gmail.com>
2024-09-15 01:27:37 +0600

    write GetPledges action with api route

--- routes/domains/pledges.php
+++ routes/domains/pledges.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use App\Domains\Pledges\Actions\Queries\GetPledge;
+use App\Domains\Pledges\Actions\Queries\GetPledges;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->prefix('pledges')->group(function (): void {
+    Route::get('/', GetPledges::class);
     Route::get('/{id}', GetPledge::class);
 });
