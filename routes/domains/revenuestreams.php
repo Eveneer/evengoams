8b287e391e71c67b51eaf76978f142b91664932a
Mahir12746 <mahiraseef@gmail.com>
2024-09-15 11:29:22 +0600

    write GetRevenueStreams Action with api route

--- routes/domains/revenuestreams.php
+++ routes/domains/revenuestreams.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
-use App\Domains\Accounts\Actions\Queries\GetAccount;
+use App\Domains\RevenueStreams\Actions\Queries\GetRevenueStream;
+use App\Domains\RevenueStreams\Actions\Queries\GetRevenueStreams;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->prefix('accounts')->group(function (): void {
-    Route::get('/{id}', GetAccount::class);
+Route::middleware(['auth:sanctum'])->prefix('revenuestreams')->group(function (): void {
+    Route::get('/', GetRevenueStreams::class);
+    Route::get('/{id}', GetRevenueStream::class);
 });
