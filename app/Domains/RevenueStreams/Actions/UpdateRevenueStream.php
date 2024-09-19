da44640069a8926aec2be030ab30db22ca79ed3e
Mahir Aseef <86533931+Mahir12746@users.noreply.github.com>
2024-09-19 12:21:51 +0600

    2024 08 07/mahir/revenuestream model operations (#65)
    * Closes #61
    
    * write model relations for revenuestream
    
    * write validation rule for createrevenuestream
    
    * write validation rule for updaterevenuestream
    
    * Closes #60
    
    * ensure code follows psr-12 standard
    
    * Closes #60
    
    * remove the input parameters from rules and create withValidator method for CreateRevenueStream and UpdateRevenueStream
    
    * write withvalidator for CreateRevenueStream and UpdateRevenueStream
    
    * Ensure PSR-12 coding style is being followed
    
    * Closes #60
    
    * write GetRevenueStream Action with api route
    
    * write GetRevenueStreams Action with api route
    
    * update GetRevenueStream

--- app/Domains/RevenueStreams/Actions/UpdateRevenueStream.php
+++ app/Domains/RevenueStreams/Actions/UpdateRevenueStream.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Domains\RevenueStreams\Actions;
 
 use App\Domains\RevenueStreams\RevenueStream;
-use Illuminate\Auth\Access\Response;
+use App\Domains\RevenueStreamTypes\RevenueStreamType;
+use App\Domains\RevenueStreamTypes\Enums\RevenueStreamTypesEnum;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Validation\Validator;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -37,17 +40,38 @@ class UpdateRevenueStream
             'name' => ['sometimes', 'string', 'max:255'],
             'description' => ['sometimes', 'nullable', 'string'],
             'type_id' => ['sometimes', 'exists:revenue_stream_types,id'],
-            'values' => ['sometimes', 'json'],
+            'values' => ['sometimes', 'array'],
         ];
+
     }
+
+    public function withValidator(Validator $validator, ActionRequest $request): void
+    {
+        $validator->after(function (Validator $validator) use ($request) {
+            if ($request->has('type_id')) {
+                $type = RevenueStreamType::find($request->type_id);
+                if ($type) {
+                    $values = $request->input('values', []);
+                    foreach ($values as $value) {
+                        $rules[$value['type']] = ['required',
+                         'in:' . implode(',', RevenueStreamTypesEnum::getValues())];
+                        }
+                    }
+                }
+            });
+    }
+
+    
 
     public function asController(RevenueStream $revenue_stream, Request $request)
     {
         return $this->handle($revenue_stream, $request->validated());
     }
 
-    public function jsonResponse(RevenueStream $revenue_stream, Request $request): array
-    {
+    public function jsonResponse(
+        RevenueStream $revenue_stream,
+         Request $request
+    ): array {
         return [
             'message' => 'RevenueStream updated successfully',
         ];
