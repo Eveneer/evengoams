72a2d5a32e26ab5dd6be6c15e46015b19a9f455f
Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>
2024-09-25 13:47:51 +0600

    Resolve merge conflicts

--- routes/domains/users.php
+++ routes/domains/users.php
@@ -2,26 +2,19 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 use App\Domains\Users\Actions\CreateUser;
+use App\Domains\Users\Actions\Queries\GetUser;
+use App\Domains\Users\Actions\Queries\GetUsers;
 use App\Domains\Users\Actions\RestoreUser;
 use App\Domains\Users\Actions\TrashUser;
 use App\Domains\Users\Actions\UpdateUser;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->prefix('users')->group(function (): void {
+    Route::get('/', GetUsers::class);
+    Route::get('/{id}', GetUser::class);
     Route::post('/', CreateUser::class);
     Route::put('/{id}', UpdateUser::class);
     Route::post('/{id}/trash', TrashUser::class);
     Route::post('/{id}/restore', RestoreUser::class);
-=======
-use App\Domains\Users\Actions\Queries\GetUser;
-use App\Domains\Users\Actions\Queries\GetUsers;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['auth:sanctum'])->prefix('users')->group(function (): void {
-    Route::get('/', GetUsers::class);
-    Route::get('/{id}', GetUser::class);
-
->>>>>>> main
 });
